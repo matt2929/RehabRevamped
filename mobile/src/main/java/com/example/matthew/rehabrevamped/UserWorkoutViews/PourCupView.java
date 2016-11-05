84c86e24dc37be6781944caa8d99d31e85c53663
matt2929 <matthewstafford29@gmail.com>
2016-11-05 15:29:21 -0400

    Updated Graphics for stability

--- mobile/src/main/java/com/example/matthew/rehabrevamped/UserWorkoutViews/PourCupView.java
+++ mobile/src/main/java/com/example/matthew/rehabrevamped/UserWorkoutViews/PourCupView.java
@@ -6,25 +6,30 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.Button;
 
 import com.example.matthew.rehabrevamped.Utilities.SampleAverage;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import static java.lang.Double.NaN;
 
 /**
  * Created by Matthew on 9/24/2016.
  */
 
 public class PourCupView extends viewabstract {
-    SampleAverage sampleAverage= new SampleAverage();
+    SampleAverage sampleAverage = new SampleAverage();
     public float percentFull;
     public Paint beerPaint, foamPaint;
     Random random = new Random();
-    float foamStart=20;
-    float gravvX=0,gravvY=0,gravvZ=0;
-    float angle=0;
-    Float gravX=-1f,gravY=-1f;
-    int spin=0;
+    float foamStart = 20;
+    float gravvX = 0, gravvY = 0, gravvZ = 0;
+    float angle = 0;
+    Float gravX = -1f, gravY = -1f;
+    int spin = 0,updateCount=0,updateMax=5;
+    ArrayList<bubbles> Bubbles= new ArrayList<bubbles>();
     public PourCupView(Context context) {
         super(context);
         initView();
@@ -47,7 +52,7 @@ public class PourCupView extends viewabstract {
 
     public void initView() {
         beerPaint = new Paint();
-        beerPaint.setColor(Color.rgb(255,191,0));
+        beerPaint.setColor(Color.rgb(255, 191, 0));
         foamPaint = new Paint();
         foamPaint.setColor(Color.WHITE);
         setBackgroundColor(Color.BLACK);
@@ -55,22 +60,59 @@ public class PourCupView extends viewabstract {
 
     @Override
     public void gimmiGrav(Float x, float y, float z) {
-        gravvX=x;
-        gravvY=y;
-        gravvZ=z;
+        gravvX = x;
+        gravvY = y;
+        gravvZ = z;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-      //  canvas.rotate(sampleAverage.getMedianAverage());
-        foamStart += (random.nextInt(10)-5);
-        if(foamStart<=0){
-            foamStart=1;
+Random random = new Random();
+        updateCount++;
+        if(updateCount==updateMax){
+            Bubbles.add(new bubbles(random.nextInt(getWidth()+10000)-5000,getHeight(),-15,15));
+            Bubbles.add(new bubbles(random.nextInt(getWidth()),getHeight(),-15,15));
+            Bubbles.add(new bubbles(random.nextInt(getWidth()),getHeight(),-15,15));
+            updateCount=0;
+        }
+
+        double angletemp=Math.atan((double) (-gravvY / gravvX));
+        if(angletemp==NaN){
+            angletemp=0;
+        }
+        Log.e("ang:", "" + (float) Math.toDegrees(angletemp));
+        if (gravX < 0) {
+            canvas.rotate((90*3)-(sampleAverage.getMedianAverage()), getWidth() / 2, getHeight() / 2);
+            Log.e("pos","left");
+
+        } else {
+            canvas.rotate(90-sampleAverage.getMedianAverage() , getWidth() / 2, getHeight() / 2);
+            Log.e("pos","right");
+        }
+        foamStart += (random.nextInt(10) - 5);
+        if (foamStart <= 0) {
+            foamStart = 1;
         }
         float waterLeftHeight = getHeight() * (percentFull);
-        canvas.drawRect(-5000,  (getHeight() - waterLeftHeight), getWidth()+5000, getHeight(), beerPaint);
-        canvas.drawRect(-5000,  (getHeight() - waterLeftHeight), getWidth()+5000, (getHeight() - waterLeftHeight)+foamStart, foamPaint);
-        Log.e("ang:",""+(float) Math.toDegrees(Math.atan((double) (-gravvY/gravvX))));
+        canvas.drawRect(-5000, (getHeight() - waterLeftHeight), getWidth() + 5000, getHeight(), beerPaint);
+        canvas.drawRect(-5000, (getHeight() - waterLeftHeight), getWidth() + 5000, (getHeight() - waterLeftHeight) + foamStart, foamPaint);
+        ArrayList<bubbles> bubTemp=new ArrayList<bubbles>();
+
+        for(bubbles b:Bubbles){
+            b.update();
+
+            canvas.drawCircle(b._X,b._Y,b._rad,foamPaint);
+            if(b._Y<getHeight()-waterLeftHeight){
+                b.set_speedY(1);
+                b.set_Y(-100);
+                bubTemp.add(b);
+            }
+
+        }
+        for(bubbles b:bubTemp){
+            Bubbles.remove(b);
+        }
+
 
     }
 
@@ -81,13 +123,41 @@ public class PourCupView extends viewabstract {
 
     @Override
     public void stringInput(String s) {
-        gravX=(float)(int)Float.parseFloat(s.split("\\,")[0]);
-        Log.e("X:",""+gravX);
-        gravY=(float)(int)(Float.parseFloat(s.split("\\,")[1]));
+        gravX = (float) (int) Float.parseFloat(s.split("\\,")[0]);
+        Log.e("X:", "" + gravX);
+        gravY = (float) (int) (Float.parseFloat(s.split("\\,")[1]));
 
-
-        Log.e("Y:",""+gravY);
-        //angle = ((float)Math.round(Math.toDegrees(Math.atan((double) (-gravY/gravX)))));
+        Log.e("Y:", "" + gravY);
+        angle = ((float) Math.round(Math.toDegrees(Math.atan((double) (gravY / gravX)))));
         sampleAverage.addSmoothAverage(angle);
+    }
+
+    public class bubbles{
+        float _X,_Y,_speedY,_rad;
+        public bubbles(float x,float y,float speedy,int radius){
+            _X=x;
+            _Y=y;
+            _speedY=speedy;
+            _rad=radius;
+        }
+        public void update(){
+            _Y+=_speedY;
+        }
+
+        public float get_rad() {
+            return _rad;
+        }
+
+        public float get_Y() {
+            return _Y;
+        }
+
+        public void set_speedY(float _speedY) {
+            this._speedY = _speedY;
+        }
+
+        public void set_Y(float _Y) {
+            this._Y = _Y;
+        }
     }
 }
